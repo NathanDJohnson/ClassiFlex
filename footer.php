fe34f2558a979520faa2074c9bdf06b01447b9f0
NathanDJohnson <nathanj.runs@gmail.com>
2015-06-12 14:42:27 -0600

    move css from footer to style.css

--- footer.php
+++ footer.php
@@ -59,64 +59,7 @@ $active_footers = 0;
 	$padding_width = $active_footers * 2;
 ?>
 <style>
-#bottom-footer-sidebar {
-  clear: both;
-  border: 0;
-  padding: 10px 20px 20px 20px;
-  margin: -20px;
-  background: black;
-}
-#bottom-footer-sidebar ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-}
-#bottom-footer-sidebar ul li {
-	display: inline;
-}
-
-#bottom-footer-sidebar ul li:after { 
-	content: " \00b7"; 
-}
-#bottom-footer-sidebar ul li:last-child:after { 
-	content: none;
-}
-
-#acurax_si_widget_simple img {
-  float: left;
-}
-
-.footer, .footer a, .footer p {
-  color: #cdcdcd;
-  text-decoration: none;
-}
-.footer a:hover {
-	opacity: 0.8;
-}
-.footer h3.widget-title {
-  color: white;
-}
 .fs{
    width: calc( <?php echo $footer_width; ?>% - <?php echo $padding_width;?>px );
-}
-
-.linearBg2 {
-  /* fallback */
-  background-color: #0B7300;
-
-  /* Safari 4-5, Chrome 1-9 */
-  background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#313131), to(#0B7300));
-
-  /* Safari 5.1, Chrome 10+ */
-  background: -webkit-linear-gradient(top, #679325, #0B7300);
-
-  /* Firefox 3.6+ */
-  background: -moz-linear-gradient(top, #679325, #0B7300);
-
-  /* IE 10 */
-  background: -ms-linear-gradient(top, #679325, #0B7300);
-
-  /* Opera 11.10+ */
-  background: -o-linear-gradient(top, #679325, #0B7300);
 }
 </style>
