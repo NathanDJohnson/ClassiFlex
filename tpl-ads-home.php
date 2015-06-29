c149103773eb531a3c435de4bd17381c3d8d2c36
NathanDJohnson <nathanj.runs@gmail.com>
2015-06-29 13:52:15 -0600

    intermediate step

--- tpl-ads-home.php
+++ tpl-ads-home.php
@@ -32,9 +32,46 @@
 			<!-- left block -->  
 			<div class="content_left">
 				<div class="recent-listings">
-					<h3>Recent Listings</h3>
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/flowchart.jpg" style="max-width:100%; height:auto;">
+			</div>
+				<div id="recent-featured-listings" class="tabcontrol" style="display: none;" >
+					<?php
+						$args = array(
+							'post_type' => APP_POST_TYPE,
+							'post_status' => 'publish',
+							'post__in' => get_option('sticky_posts'),
+							'posts_per_page' => 4,
+							'orderby' => 'date',
+							'suppress_filters' => false,
+						);
+ 
+						$args = apply_filters( 'cp_featured_slider_args', $args );
+ 						$featured = new WP_Query( $args ); ?>
+ 						
+					 <?php if ( $featured->have_posts() ) : while ( $featured->have_posts() ) : $featured->the_post(); ?>
+<div class="post-wrapper">
+	<div class="post-image">
+	<?php if ( $cp_options->ad_images ) cp_ad_loop_thumbnail(); ?>
+	</div>
+	<div class="post-description">
+		<div class="post-head">
+			<h3>
+				<a href="<?php the_permalink(); ?>"><?php if ( mb_strlen( get_the_title() ) >= 75 ) echo mb_substr( get_the_title(), 0, 75 ) . '...'; else the_title(); ?></a>
+			</h3>
+			<?php appthemes_before_post_title(); // price tag?>
+		</div>
+		<p class="post-content"><?php echo cp_get_content_preview( 150 ); ?></p>
+		<?php //appthemes_after_post_title(); //post meta ?>
+		<?php //appthemes_before_post_content(); // ? ?>
+	</div>
+</div>
+ <?php endwhile; 
+ wp_reset_postdata();
+ else : ?>
+ <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+ <?php endif; ?>
 				</div>
-				<div class="tabcontrol">
+				<div class="tabcontrol" style="display:none;">
 					<?php
 						remove_action( 'appthemes_after_endwhile', 'cp_do_pagination' );
 						$post_type_url = add_query_arg( array( 'paged' => 2 ), get_post_type_archive_link( APP_POST_TYPE ) );
