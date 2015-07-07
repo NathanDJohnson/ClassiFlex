b65012f97a6170824b39f75b59ef98e289e9dd3c
NathanDJohnson <nathanj.runs@gmail.com>
2015-07-07 13:02:00 -0600

    edit author.php

--- author.php
+++ author.php
@@ -10,6 +10,9 @@
 //This sets the $curauth variable
 $curauth = get_queried_object();
 
+$authtype = get_user_meta( get_the_author_meta('ID'), 'active_membership_pack' );
+$authtype = $authtype[0];
+
 $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 $author_posts_count = count_user_posts( $curauth->ID );
 ?>
@@ -27,6 +30,7 @@ $author_posts_count = count_user_posts( $curauth->ID );
 							<div class="author-main">
 								<ul class="author-info">
 									<li><strong><?php _e( 'Member Since:', APP_TD ); ?></strong> <?php echo appthemes_display_date( $curauth->user_registered, 'date', true ); ?></li>
+									<li><strong>Membership:</strong> <?php echo $authtype; ?></li>
 									<?php if ( ! empty( $curauth->user_url ) ) { ?><li><strong><?php _e( 'Website:', APP_TD ); ?></strong> <a href="<?php echo esc_url( $curauth->user_url ); ?>"><?php echo strip_tags( $curauth->user_url ); ?></a></li><?php } ?>
 									<?php if ( ! empty( $curauth->twitter_id ) ) { ?><li><div class="twitterico"></div><a href="http://twitter.com/<?php echo urlencode( $curauth->twitter_id ); ?>" target="_blank"><?php _e( 'Twitter', APP_TD ); ?></a></li><?php } ?>
 									<?php if ( ! empty( $curauth->facebook_id ) ) { ?><li><div class="facebookico"></div><a href="<?php echo appthemes_make_fb_profile_url( $curauth->facebook_id ); ?>" target="_blank"><?php _e( 'Facebook', APP_TD ); ?></a></li><?php } ?>
@@ -39,28 +43,12 @@ $author_posts_count = count_user_posts( $curauth->ID );
 					</div><!-- /shadowblock -->
 				</div><!-- /shadowblock_out -->
 				<div class="tabcontrol">
-					<ul class="tabnavig">
-						<li><a href="#block1"><span class="big"><?php _e( 'Ads', APP_TD ); ?></span></a></li>
-						<?php if ( $author_posts_count ) { ?>
-							<li><a href="#block2"><span class="big"><?php _e( 'Posts', APP_TD ); ?></span></a></li>
-						<?php } ?>
-					</ul>
-					<!-- tab 1 -->
 					<div id="block1">
 						<div class="clr"></div>
 						<div class="undertab"><span class="big"><?php _e( 'Ads', APP_TD ); ?> / <strong><span class="colour"><?php _e( 'Latest items listed', APP_TD ); ?></span></strong></span></div>
 						<?php query_posts( array( 'post_type' => APP_POST_TYPE, 'author' => $curauth->ID, 'paged' => $paged ) ); ?>
 						<?php get_template_part( 'loop', 'ad_listing' ); ?>
 					</div><!-- /block1 -->
-					<?php if ( $author_posts_count ) { ?>
-					<!-- tab 2 -->
-					<div id="block2">
-						<div class="clr"></div>
-						<div class="undertab"><span class="big"><?php _e( 'Posts', APP_TD ); ?> / <strong><span class="colour"><?php _e( 'Recent blog posts', APP_TD ); ?></span></strong></span></div>
-						<?php query_posts( array( 'post_type' => 'post', 'author' => $curauth->ID, 'paged' => $paged ) ); ?>
-						<?php get_template_part( 'loop' ); ?>
-					</div><!-- /block2 -->
-					<?php } ?>
 				</div><!-- /tabcontrol -->
 			</div><!-- /content_left -->
 			<?php get_sidebar( 'author' ); ?>
