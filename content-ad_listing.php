0dc36092d6aca8153b89f8161e7559807426bfe6
NathanDJohnson <nathanj.runs@gmail.com>
2015-07-08 22:59:46 -0600

    edit content-ad_listing.php

--- content-ad_listing.php
+++ content-ad_listing.php
@@ -21,7 +21,7 @@ global $cp_options;
 		<div class="post-content">
 			<p class="post-content">
 			<?php
-				if( cpc_author_membership_pack( get_the_author_meta('ID') ) == 'Featured Broker' && get_post_meta( get_the_ID(), 'cp_broker_description', true ) != false ) {
+				if( cpc_author_membership_pack( get_the_author_meta('ID') ) == 'Featured Broker' && get_post_meta( get_the_ID(), 'cp_broker_description', true ) != '' ) {
 					echo get_post_meta( get_the_ID(), 'cp_broker_description', true );
 				}
 				else{
