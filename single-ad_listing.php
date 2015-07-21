47807d6abb7c673f658d02b4aa5f39719833ec41
NathanDJohnson <nathanj.runs@gmail.com>
2015-07-21 16:12:54 -0600

    show ad image(s) if is_sticky()

--- single-ad_listing.php
+++ single-ad_listing.php
@@ -44,7 +44,7 @@
 									<?php } ?>
 									</ul>
 								</div><!-- /bigright -->
-								<?php if ( $cp_options->ad_images && cpc_is_featured_description( get_the_author_meta('ID') ) ) { ?>
+								<?php if ( $cp_options->ad_images && cpc_is_featured_description( get_the_author_meta('ID') ) || is_sticky() ) { ?>
 									<div class="bigleft">
 										<div id="main-pic">
 											<?php cp_get_image_url(); ?>
