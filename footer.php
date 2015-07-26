63707ce2d38fda3f1ae345cb19da8f7a4cd3de4c
NathanDJohnson <nathanj.runs@gmail.com>
2015-07-26 13:12:32 -0600

    cleanup footer.php

--- footer.php
+++ footer.php
@@ -12,61 +12,43 @@ $active_footers = 0;
 ?>
 <div class="footer">
 	<div id="footer-sidebar" class="secondary">
-	<?php
-		if(is_active_sidebar('footer-sidebar-1')){
-	?>
+	<?php if(is_active_sidebar('footer-sidebar-1')) : ?>
 		<div id="footer-sidebar1" class="fs">
 		<?php
 				$active_footers = $active_footers + 1;
 				dynamic_sidebar('footer-sidebar-1');
 		?>
 		</div><!-- #footer-sidebar1 -->
-	<?php
-		}
-	?>
-	<?php
-		if(is_active_sidebar('footer-sidebar-2')){
-	?>
+	<?php endif; ?>
+	<?php if(is_active_sidebar('footer-sidebar-2')) : ?>
 		<div id="footer-sidebar2" class="fs">
 		<?php
 				$active_footers = $active_footers + 1;
 				dynamic_sidebar('footer-sidebar-2');
 		?>
 		</div><!-- #footer-sidebar2 -->
-	<?php
-		}
-	?>
-	<?php
-		if(is_active_sidebar('footer-sidebar-3')){
-	?>
+	<?php endif; ?>
+	<?php if(is_active_sidebar('footer-sidebar-3')) : ?>
 		<div id="footer-sidebar3" class="fs">
 		<?php
 				$active_footers = $active_footers + 1;
 				dynamic_sidebar('footer-sidebar-3');
 		?>
 		</div><!-- #footer-sidebar3 -->
-	<?php
-		}
-	?>
-	<?php
-		if(is_active_sidebar('footer-sidebar-4')){
-	?>
+	<?php endif; ?>
+	<?php if(is_active_sidebar('footer-sidebar-4')) : ?>
 		<div id="footer-sidebar4" class="fs">
 		<?php
 				$active_footers = $active_footers + 1;
 				dynamic_sidebar('footer-sidebar-4');
 		?>
 		</div><!-- #footer-sidebar4 -->
-	<?php
-		}
-	?>
-	<div id="bottom-footer-sidebar" class="">
-	<?php
-		if(is_active_sidebar('sidebar_footer')){
-			dynamic_sidebar('sidebar_footer');
-		}
-	?>
-	</div>
+	<?php endif; ?>
+	<?php if(is_active_sidebar('sidebar_footer')) : ?> 
+		<div id="bottom-footer-sidebar" class="">
+			<?php dynamic_sidebar('sidebar_footer'); ?>
+		</div>
+	<?php endif; ?>
 </div><!-- /footer -->
 <?php
 	// Calculate width of footers here
