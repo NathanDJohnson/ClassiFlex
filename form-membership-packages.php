67feac174ea45103fc163d4eead0197349539107
NathanDJohnson <nathanj.runs@gmail.com>
2015-07-30 14:30:34 -0600

    modify table

--- form-membership-packages.php
+++ form-membership-packages.php
@@ -14,7 +14,7 @@
 			<div class="shadowblock_out">
 				<div class="shadowblock">
 					<div id="step1">
-						<h2 class="dotted"><?php _e( 'Purchase a Membership Pack', APP_TD ); ?></h2>
+						<h2 class="dotted"><?php _e( 'Select Membership', APP_TD ); ?></h2>
 						<img src="<?php echo appthemes_locate_template_uri( 'images/step1.gif' ); ?>" alt="" class="stepimg" />
 						<?php do_action( 'appthemes_notices' ); ?>
 						<p class="dotted">&nbsp;</p>
@@ -24,15 +24,15 @@
 								<table id="memberships" class="widefat fixed footable">
 									<thead style="text-align:left;">
 										<tr>
-											<th scope="col" data-class="expand"><?php _e( 'Name', APP_TD ); ?></th>
-											<th scope="col" data-hide="phone"><?php _e( 'Membership Benefit', APP_TD ); ?></th>
-											<th scope="col" data-hide="phone"><?php _e( 'Price', APP_TD ); ?></th>
+											<th scope="col" data-class="expand"><?php _e( 'Member Level', APP_TD ); ?></th>
+											<th scope="col" data-hide="phone"><?php _e( 'Description', APP_TD ); ?></th>
+											<th scope="col" data-hide="phone"><?php _e( 'Subscription', APP_TD ); ?></th>
 											<th scope="col" style="width:75px;" data-hide="phone"></th>
 										</tr>
 									</thead>
 								<?php
 									if ( $packages ) {
-										usort($packages, "fmp_cmpDescA");
+										//usort($packages, "fmp_cmpDescA");
 								?>
 										<tbody id="list">
 										<?php
@@ -70,6 +70,22 @@
 														}
 														else{
 															$paynow = "Pay Now";
+														}
+													?>
+													<?php
+														/* Submit Button Text */
+														switch ( $package->pack_name ) {
+														case 'Premium Broker':
+															$paynow = "Select Premium";
+															break;
+														case 'Featured Broker':
+															$paynow = "Select Featured";
+															break;
+														case 'Broker':
+															$paynow = "Select Standard";
+															break;
+														default:
+															$paynow = "Free Non-Member";
 														}
 													?>
 													<td><input type="submit" name="step1" id="step1" class="btn_orange" onclick="document.getElementById('pack').value=<?php echo $package->ID; ?>;" value="<?php echo $paynow; ?>" style="margin-left: 5px; margin-bottom: 5px;" /></td>
