1291a6d4cd75424e55c71d8fa298359dae3e76eb
NathanDJohnson <nathanj.runs@gmail.com>
2015-08-03 18:18:15 -0600

    Update edit-listing.php

--- edit-listing.php
+++ edit-listing.php
@@ -6,6 +6,14 @@
  * @author  AppThemes
  * @since   ClassiPress 3.4
  */
-
+?>
+<?php if( !cpc_is_featured_description( get_current_user_id() ) ) : ?>
+<style>
+	#list_cp_broker_description {
+		display: none;
+	}
+</style>
+<?php endif; ?>
+<div class="clr" style="margin-top:10px;"></div>
+<?php
 appthemes_display_checkout();
-
