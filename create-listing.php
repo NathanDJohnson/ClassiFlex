17d31851d3a73d28db9f144f7fdd358853c4baa1
NathanDJohnson <nathanj.runs@gmail.com>
2015-08-11 10:49:26 -0600

    more styling

--- create-listing.php
+++ create-listing.php
@@ -7,13 +7,13 @@
  * @since   ClassiPress 3.4
  */
 ?>
-<?php if( !cpc_is_featured_description( get_current_user_id() ) ) : ?>
 <style>
-	#list_cp_broker_description {
-		display: none;
-	}
-</style>
+	p.dotted{ color: #fff; }
+	.notice.success{ display: none; }
+<?php if( !cpc_is_featured_description( get_current_user_id() ) ) : ?>
+	#list_cp_broker_description { display: none; }
 <?php endif; ?>
+</style>
 <script type='text/javascript'>jQuery(document).ready(function(){$('.dotted').each(function() {$(this).html('Create Your Listing');});});</script>
 <div class="clr" style="margin-top:10px;"></div>
 <?php
